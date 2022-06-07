7ca7ae987d37c81d034e9a59095b15e8afd6a931
David Arroyo Moreno <david.arroyo-ext@adentia.es>
2022-06-07 15:55:54 +0200

    v1.5.5

--- firestore_functions/functions/src/index.ts
+++ firestore_functions/functions/src/index.ts
@@ -19,17 +19,17 @@ export const searchDictionary = functions.region("europe-west1")
       JSON.parse(req.body.data).words || "";
 
       //  Creating a personalised response object and a documents array
-      let response = new Response();
+      let wres = new WordResponse();
       let documents: Array<DocumentData> = [];
 
       //  If the search result matches exactly the result, we send
       //  a flag back to the client to implement a new functionality
-      const perfectQuery = await db.collection("words")
+      const perfectQuery = await db.collection("dictionary")
           .where("words", "==", words).get();
 
       if (perfectQuery.docs.length > 0) {
-        response.data.exactMatch = true;
-        documents.push(perfectQuery.docs);
+        wres.exactMatch = true;
+        documents.push(...perfectQuery.docs);
       } else {
         documents = await searchAlgorithm(words);
       }
@@ -38,32 +38,31 @@ export const searchDictionary = functions.region("europe-west1")
       //  then we call dictionaryGenerator() to make one
       if (documents.length === 0) {
         let document: DocumentData;
-        response = await callCloudFunction("dictionaryGenerator", words);
+        wres = await callCloudFunction("dictionaryGenerator", words);
         //  If there is an error when creating the document, try a second time
-        if (response.data.errorCode !== -1) {
-          response = await callCloudFunction("dictionaryGenerator", words);
+        if (wres.errorCode !== -1) {
+          wres = await callCloudFunction("dictionaryGenerator", words);
         }
         //  If the document is finally created succesfully, then we proceed
-        if (response.data.errorCode === -1) {
-
+        if (wres.errorCode === -1) {
           //  POR HACER --> El codigo en GCP se para aqui (linea de mas abajo),
           //  averiguar como solucionar esto
-          functions.logger.warn(`RESPONSE CONTENTS: ${response.data.contents}`);
-          document = response.data.contents;
+          functions.logger.warn(`RESPONSE OBJECT: ${wres}`);
+          document = wres.docs[0];
 
           //  After having created a document, we store it in the database
           await db.collection("dictionary").add(document)
               .catch((error) => {
-                response.data.error = `ERROR IN FIRESTORE: ${error}`;
-                response.data.errorCode = 8;
+                wres.error = `ERROR IN FIRESTORE: ${error}`;
+                wres.errorCode = 8;
               });
           documents.push(document);
         }
       }
 
       //  Finally we return the response to the client
-      response.data.contents = documents;
-      res.status(200).send(response);
+      wres.docs.push(...documents);
+      res.status(200).send(wres);
     });
 
 
@@ -74,6 +73,13 @@ export async function searchAlgorithm(words: string)
   //  First of all, we create the subcombinations of 2 words
   //  for the req text that has been sent from the client
   const combinations = makeCombinations(words);
+
+  //  Special case for when the search is composed of only one word
+  if (combinations.length === 0) {
+    const query = await db.collection("dictionary")
+        .where("words", "==", words).get();
+    return query.docs;
+  }
 
   //  Firestore's arrayContainsAny() method only allows querying
   //  for 10 elements each time it is called, so we have to divide
@@ -90,7 +96,7 @@ export async function searchAlgorithm(words: string)
   //  (first 1/3, unoptimised)
   groups.forEach(async (group: Array<string>, i: number) => {
     if (i < Math.floor(groups.length/3)) {
-      const query = db.collection("words")
+      const query = db.collection("dictionary")
           .where("combinations", "array-contains-any", group);
       firstBatch.push(query.get());
     }
@@ -100,7 +106,7 @@ export async function searchAlgorithm(words: string)
       .then((snapshots) => {
         //  We pull out the documents from the query snapshots
         snapshots.forEach((snapshot) => {
-          documents.push(snapshot.docs);
+          documents.push(...snapshot.docs);
         });
         //  This line of code erases any duplicate documents
         documents = [...new Set(documents)];
@@ -110,7 +116,7 @@ export async function searchAlgorithm(words: string)
   //  (last 2/3, optimised hopefully)
   groups.forEach(async (group: Array<string>, i: number) => {
     if (i >= Math.floor(groups.length/3) ) {
-      let query = db.collection("words")
+      let query = db.collection("dictionary")
           .where("combinations", "array-contains-any", group);
       //  We optimise the subsequent queries to return the least
       //  amount of duplicate data possible and in turn save more money
@@ -124,7 +130,7 @@ export async function searchAlgorithm(words: string)
   await Promise.all(secondBatch)
       .then((snapshots) => {
         snapshots.forEach((snapshot) => {
-          documents.push(snapshot.docs);
+          documents.push(...snapshot.docs);
         });
         documents = [...new Set(documents)];
       });
@@ -179,8 +185,7 @@ export function optimiseQuery(query: Query<DocumentData>,
 //  Calls any of your cloud functions and returns its response
 export async function callCloudFunction(name: string, message: string)
 : Promise<any> {
-  const response = new Response();
-  // response.data.contents = new WordDocument(data);
+  const wres = new WordResponse();
   const url = `https://europe-west1-candle-9cfbb.cloudfunctions.net/${name}`;
   await fetch(url, {method: "POST",
     headers: {
@@ -189,74 +194,33 @@ export async function callCloudFunction(name: string, message: string)
     body: JSON.stringify({"words": message})}
   )
       .then(async (result) => {
-        const res = await result.text();
-        //  Esta linea es la que parece dar problemas en GCF:
-        //  response.data = JSON.parse(res).data;
-
-        //  POR HACER --> esta linea de abajo hay que arreglarla
-        //  para que el objeto response tenga un formato adecuado,
-        //  sin los campos de "error" y "errorCode" duplicados
-        response.data.contents = res;
+        //  FUNCIONARA? (POR HACER)
+        const doc = await result.json() as DocumentData;
+        wres.docs.push(doc);
       })
       .catch((error) => {
-        response.data.error = `ERROR CALLING CLOUD FUNCTION: ${error}`;
-        response.data.errorCode = 7;
+        wres.error = `ERROR CALLING CLOUD FUNCTION: ${error}`;
+        wres.errorCode = 7;
 
         //  La ultima linea sirve para loguear informacion a GCP
-        const message = JSON.stringify(response.data);
-        functions.logger.warn("RESPONSE CONTENTS:", { message });
+        const message = JSON.stringify(wres);
+        functions.logger.warn("RESPONSE CONTENTS:", {message});
       });
-  return response;
-}
-
-
-export class WordDocument {
-  //  Fields
-  words: string;
-  wordCount: number;
-  types: Array<string>;
-  meanings: Array<string>;
-  synonyms: Array<string>;
-  translations: Array<string>;
-  examples: Array<string>;
-  combinations: Array<string>;
-
-  //  Constructor
-  constructor(words: string) {
-    this.words=words;
-    this.wordCount=words.split(/\s/gm).length;
-    this.types=[];
-    this.meanings=[];
-    this.synonyms=[];
-    this.translations=[];
-    this.examples=[];
-    this.combinations=[];
-  }
+  return wres;
 }
 
 
 //  Constructor for the response object
-export class Response {
+export class WordResponse {
   // Fields
-  data: Data;
-
-  // Constructor
-  constructor() {
-    this.data=new Data();
-  }
-}
-
-
-export class Data {
-  // Fields
-  contents: any;
+  docs: Array<DocumentData>;
   error: string;
   errorCode: number;
   exactMatch: boolean;
 
   // Constructor
   constructor() {
-    this.contents=null;
+    this.docs=[];
     this.error="";
     this.errorCode=-1;
     this.exactMatch=false;
